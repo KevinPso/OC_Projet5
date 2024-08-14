cb5a8c0e0e7f847f23cb5593223fb40a3ee4d858
Kevin Pso <kevin.pso@proton.me>
2024-08-14 21:10:35 +0200

    Gestion du Responsive SASS pour chaque page et chaque composant, ajout de NavLink au Logo

--- kasa/src/components/Header.jsx
+++ kasa/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import "../styles/styles.scss"
 import logo from "../assets/logoKasa.png"
 import Nav from "./Nav"
@@ -7,7 +8,9 @@ const Header = () => {
   return (
     <header className="header">
       <h1>
+      <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
         <img src={logo} alt="Logo de Kasa" />
+      </NavLink>
       </h1>
       <Nav />
     </header>
